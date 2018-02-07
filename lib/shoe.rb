80366bcdf19c0a7a23e335db4ece0b136bdb812b
Fiona Lapham <fionadlapham@gmail.com>
2018-02-07 16:04:52 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material
+  def initialize (brand)
+    @brand = brand
+  end
+  def cobble
+    puts "The show has been repaired."
+  end
+end
